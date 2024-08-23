e38582df875f073b111eec27fdb4b941d6ff7277
Shobhit Chaurasia <shobhitchaurasia@Shobhits-MacBook-Air.local>
2024-08-23 22:28:24 +0530

    Refactor and added UI for taking input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'string-calculator-tdd-kata';
+  inputString: string = '';
+  result: number | null = null;
+  errorMessage: string | null = null;
 
   add(numbers: string): number {
-    if (numbers === "") return 0;
+    if (numbers === '') return 0;
 
+    const [delimiter, numberStr] = this.extractDelimiters(numbers);
+
+    const numArray = numberStr.split(delimiter).map((num) => parseInt(num));
+    const negatives = numArray.filter((num) => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negatives not allowed: ${negatives.join(',')}`);
+    }
+    return numArray.filter((num) => num <= 1000).reduce((a, b) => a + b, 0);
+  }
+
+  extractDelimiters(numbers: string): [RegExp, string] {
     let delimiter = /[\n,]/;
 
-    if (numbers.startsWith("//")) {   // supports different delimiters
+    if (numbers.startsWith('//')) {
+      // supports different delimiters
       const parts = numbers.split('\n');
       const delimiterPart = parts[0];
 
-      if(delimiterPart.startsWith("//[")) {
+      if (delimiterPart.startsWith('//[')) {
         const delimiterContent = delimiterPart
           .slice(2) // it will remove initial "//"
           .split('][') // it will split by "][" to handle multiple delimiters
-          .map(d => d.replace(/[\[\]]/g, '')) // it will remove square brackets
-          .map(d => d.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')) // it will escape special characters
+          .map((d) => d.replace(/[\[\]]/g, '')) // it will remove square brackets
+          .map((d) => d.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')) // it will escape special characters
           .join('|'); // it will join them using OR operator for regex
         delimiter = new RegExp(delimiterContent);
       } else {
@@ -31,12 +46,16 @@ export class AppComponent {
 
       numbers = parts[1];
     }
+    return [delimiter, numbers];
+  }
 
-    const numArray = numbers.split(delimiter).map(num => parseInt(num));
-    const negatives = numArray.filter(num => num < 0);
-    if (negatives.length > 0) {
-      throw new Error(`negatives not allowed: ${negatives.join(",")}`);
+  calculateSum(): void {
+    try {
+      this.result = this.add(this.inputString);
+      this.errorMessage = null;
+    } catch (error: any) {
+      this.result = null;
+      this.errorMessage = error.message;
     }
-    return numArray.filter(num => num <= 1000).reduce((a, b) => a + b, 0);
   }
 }
